6071ea9426bd2b0e9015f929e4ed2cb2abe71877
steveking <steveking@98b43a4e-47f0-47d6-ba00-cf671fc20f81>
2008-01-02 14:02:30 +0000

    Tag the 1.2.2 release

---
+++ tags/version-1.2.2/src/BookmarksDlg.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "basedialog.h"
+#include "DlgResizer.h"
+#include "Bookmarks.h"
+#include <string>
+
+using namespace std;
+
+/**
+ * bookmarks dialog.
+ */
+class CBookmarksDlg : public CDialog
+{
+public:
+	CBookmarksDlg(HWND hParent);
+	~CBookmarksDlg(void);
+
+	wstring					GetName() {return m_name;}
+	wstring					GetSelectedSearchString() {return m_searchString;}
+	wstring					GetSelectedReplaceString() {return m_replaceString;}
+	bool					GetSelectedUseRegex() {return m_bUseRegex;}
+
+protected:
+	LRESULT CALLBACK		DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	LRESULT					DoCommand(int id, int msg);
+	void					InitBookmarks();
+private:
+	HWND					m_hParent;
+	wstring					m_name;
+	CBookmarks				m_bookmarks;
+
+	wstring					m_searchString;
+	wstring					m_replaceString;
+	bool					m_bUseRegex;
+
+	CDlgResizer				m_resizer;
+};
