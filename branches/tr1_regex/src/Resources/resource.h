6dead401154ed62b8c982e9c2885fbe519c86e7d
steveking <steveking@98b43a4e-47f0-47d6-ba00-cf671fc20f81>
2008-05-25 09:13:53 +0000

    create branch to store attempt to use the tr1::regex library instead of the boost::regex one.

---
+++ branches/tr1_regex/src/Resources/resource.h
@@ -0,0 +1,75 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by GrepWin.rc
+//
+#define IDS_APP_TITLE                   103
+#define IDI_GREPWIN                     107
+#define IDC_GREPWIN                     109
+#define IDD_SEARCHDLG                   129
+#define IDD_REGEXTEST                   130
+#define IDD_NAME                        131
+#define IDD_BOOKMARKS                   132
+#define IDR_MENU1                       133
+#define IDC_BKPOPUP                     133
+#define IDR_INFODLG                     134
+#define IDD_ABOUT                       135
+#define IDR_ACCELERATOR1                136
+#define IDR_SEARCHDLG                   136
+#define IDC_SEARCHTEXT                  1000
+#define IDC_REGEXRADIO                  1001
+#define IDC_TEXTRADIO                   1002
+#define IDC_REGEXOKLABEL                1004
+#define IDC_ALLSIZERADIO                1005
+#define IDC_SIZERADIO                   1006
+#define IDC_SIZECOMBO                   1007
+#define IDC_SIZEEDIT                    1008
+#define IDC_INCLUDESYSTEM               1009
+#define IDC_INCLUDEHIDDEN               1010
+#define IDC_INCLUDESUBFOLDERS           1011
+#define IDC_SEARCHPATH                  1012
+#define IDC_SEARCHPATHBROWSE            1013
+#define IDC_RESULTLIST                  1014
+#define IDC_GROUPSEARCHIN               1015
+#define IDC_GROUPSEARCHFOR              1016
+#define IDC_GROUPLIMITSEARCH            1017
+#define IDC_GROUPSEARCHRESULTS          1018
+#define IDC_KBTEXT                      1019
+#define IDC_SEARCHINFOLABEL             1020
+#define IDC_ADDTOBOOKMARKS              1021
+#define IDC_BOOKMARKS                   1022
+#define IDC_REPLACETEXT                 1023
+#define IDC_SEARCHFORLABEL              1026
+#define IDC_REPLACEWITHLABEL            1027
+#define IDC_TESTREGEX                   1028
+#define IDC_CREATEBACKUP                1029
+#define IDC_TEXTCONTENT                 1030
+#define IDC_REGEXMATCH                  1031
+#define IDC_REGEXREPLACED               1032
+#define IDC_NAME                        1037
+#define IDC_PATTERNLABEL                1039
+#define IDC_EDIT1                       1040
+#define IDC_PATTERN                     1040
+#define IDC_CHECK1                      1042
+#define IDC_CASE_SENSITIVE              1042
+#define IDC_VERSIONINFO                 1043
+#define IDC_DATE                        1044
+#define IDC_WEBLINK                     1045
+#define IDC_FILEPATTERNREGEX            1046
+#define IDC_FILEPATTERNTEXT             1048
+#define IDC_REPLACE                     1049
+#define IDC_CHECK2                      1050
+#define IDC_INCLUDEBINARY               1050
+#define ID_REMOVEBOOKMARK               32771
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1051
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
