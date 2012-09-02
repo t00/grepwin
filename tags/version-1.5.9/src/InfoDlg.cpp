9db2d3195d8167d4acd13149384dccec53a11b57
steveking <steveking@98b43a4e-47f0-47d6-ba00-cf671fc20f81>
2012-09-02 11:04:49 +0000

    tag the 1.5.9 release

---
+++ tags/version-1.5.9/src/InfoDlg.cpp
@@ -0,0 +1,79 @@
+// grepWin - regex search and replace for Windows
+
+// Copyright (C) 2007-2009, 2011-2012 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "InfoDlg.h"
+#include "maxpath.h"
+
+#include <memory>
+#include <mshtmhst.h>
+
+#pragma comment(lib, "Urlmon.lib")
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CInfoDlg::CInfoDlg()
+{
+
+}
+
+CInfoDlg::~CInfoDlg()
+{
+
+}
+
+//Function which takes input of An HTML Resource Id
+BOOL CInfoDlg::ShowDialog(UINT idAboutHTMLID, HINSTANCE hInstance)
+{
+    //Load the IE Specific MSTML Interface DKK
+    HINSTANCE   hinstMSHTML = LoadLibrary(TEXT("MSHTML.DLL"));
+    BOOL bSuccess = FALSE;
+    if(hinstMSHTML)
+    {
+        SHOWHTMLDIALOGFN  *pfnShowHTMLDialog;
+        //Locate The Function ShowHTMLDialog in the Loaded MSHTML.DLL
+        pfnShowHTMLDialog =     (SHOWHTMLDIALOGFN*)GetProcAddress(hinstMSHTML, "ShowHTMLDialog");
+        if(pfnShowHTMLDialog)
+        {
+            std::unique_ptr<TCHAR[]> lpszModule(new TCHAR[MAX_PATH_NEW]);
+            //Get The Application Path
+            if (GetModuleFileName(hInstance, lpszModule.get(), MAX_PATH_NEW))
+            {
+                //Add the IE Res protocol
+                std::unique_ptr<TCHAR[]> strResourceURL(new TCHAR[MAX_PATH_NEW]);
+                _stprintf_s(strResourceURL.get(), MAX_PATH_NEW, _T("res://%s/%d"), lpszModule.get(), idAboutHTMLID);
+                int iLength = (int)_tcslen(strResourceURL.get());
+                std::unique_ptr<wchar_t[]> lpWideCharStr(new wchar_t[iLength+1]);
+                //Attempt to Create the URL Moniker to the specified in the URL String
+                IMoniker *pmk;
+                if(SUCCEEDED(CreateURLMoniker(NULL,strResourceURL.get(),&pmk)))
+                {
+                    //Invoke the ShowHTMLDialog function by pointer
+                    //passing the HWND of your Application , the Moniker,
+                    //the remaining parameters can be set to NULL
+                    pfnShowHTMLDialog(NULL,pmk,NULL,L"resizable:yes",NULL);
+                    bSuccess = TRUE;
+                }
+            }
+        }
+        FreeLibrary(hinstMSHTML);
+    }
+    return bSuccess;
+}
---
+++ tags/version-1.5.9/src/NameDlg.cpp
@@ -0,0 +1,96 @@
+// grepWin - regex search and replace for Windows
+
+// Copyright (C) 2007-2008, 2012 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "Resource.h"
+#include "NameDlg.h"
+#include <string>
+
+#include <boost/regex.hpp>
+
+
+CNameDlg::CNameDlg(HWND hParent)
+{
+    m_hParent = hParent;
+}
+
+CNameDlg::~CNameDlg(void)
+{
+}
+
+LRESULT CNameDlg::DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+        {
+            InitDialog(hwndDlg, IDI_GREPWIN);
+            // initialize the controls
+
+            SetDlgItemText(*this, IDC_NAME, m_name.c_str());
+            SetFocus(GetDlgItem(hwndDlg, IDC_NAME));
+
+            m_resizer.Init(hwndDlg);
+
+            ExtendFrameIntoClientArea(IDC_NAME, IDC_NAME, IDC_NAME, IDC_NAME);
+            m_aerocontrols.SubclassControl(GetDlgItem(*this, IDOK));
+            m_aerocontrols.SubclassControl(GetDlgItem(*this, IDCANCEL));
+            m_aerocontrols.SubclassControl(GetDlgItem(*this, IDC_INFOLABEL));
+            if (m_Dwm.IsDwmCompositionEnabled())
+                m_resizer.ShowSizeGrip(false);
+        }
+        return FALSE;
+    case WM_COMMAND:
+        return DoCommand(LOWORD(wParam), HIWORD(wParam));
+    case WM_SIZE:
+        {
+            m_resizer.DoResize(LOWORD(lParam), HIWORD(lParam));
+        }
+        break;
+    case WM_GETMINMAXINFO:
+        {
+            MINMAXINFO * mmi = (MINMAXINFO*)lParam;
+            mmi->ptMinTrackSize.x = m_resizer.GetDlgRect()->right;
+            mmi->ptMinTrackSize.y = m_resizer.GetDlgRect()->bottom;
+            return 0;
+        }
+        break;
+    default:
+        return FALSE;
+    }
+    return FALSE;
+}
+
+LRESULT CNameDlg::DoCommand(int id, int /*msg*/)
+{
+    switch (id)
+    {
+    case IDOK:
+        {
+            auto buf = GetDlgItemText(IDC_NAME);
+            m_name = buf.get();
+        }
+        // fall through
+    case IDCANCEL:
+        EndDialog(*this, id);
+        break;
+    }
+    return 1;
+}
+
